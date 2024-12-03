209ca58b3764a2ec90f33b66c36e495965102e67
Dmytro <dmytronasikan@gmail.com>
2024-12-03 15:16:13 +0200

    removed expired secrets

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIATWBJ2BBBUHV7TQIN"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 terraform {
